55e135c8dbe5e88075c6cfd04df7ffc03362cc71
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-09-11 15:52:08 +0500

    changess

---
+++ deploy-beta.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+​
+echo "Deploying application ..."
+​
+# Enter maintenance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git checkout beta
+    git reset --hard origin/beta
+    git pull origin beta
+​
+    cd /var/www/html
+    cp -R /var/repos/aspca/* /var/www/html
+​
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+​
+    # Clear cache
+    php artisan config:cache
+    php artisan config:clear
+    php artisan optimize:clear
+​
+    # Migrate database
+    php artisan migrate
+​
+    # Note: If you're using queue workers, this is the place to restart them.
+    # ...
+    php artisan queue:restart
+    
+    composer dumpautoload
+​
+​
+# Exit maintenance mode
+php artisan up
+​
+echo "Application deployed!"
