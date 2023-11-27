20bd76dad23cbfe114eb5bf2442b061f2729b266
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-11-27 16:59:15 +0500

    changes

--- app/Http/Controllers/AcademicTestController.php
+++ app/Http/Controllers/AcademicTestController.php
@@ -11,7 +11,7 @@ class AcademicTestController extends Controller
 
     public function getAcademicTest(Request $request){
      
-        $tests = Test::where('"category"',1)->where('type',$request->type)->get();
+        $tests = Test::where('category',1)->where('type',$request->type)->get();
         
         return view('frontend.pages.academic-test',compact('tests'));
     }
