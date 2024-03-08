01cadadb108d044b700cbc84ced997b0eee70c8c
Maheer <86462580+okmaheer@users.noreply.github.com>
2024-03-08 19:06:09 +0500

    changes

--- app/Http/Controllers/RegisterationRequestController.php
+++ app/Http/Controllers/RegisterationRequestController.php
@@ -33,7 +33,7 @@ class RegisterationRequestController extends Controller
     public function index()
     {
 
-        $requests = RegisterationRequest::orderBy('asc','created_at')->get();
+        $requests = RegisterationRequest::orderBy('created_at','asc')->get();
         return view('admin.requests.index', compact('requests'));
     }
     public function getCountries()
